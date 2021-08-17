2a842291899f179f9f847c10935c448e33e7efd2
Crowdbotics <team@crowdbotics.com>
2021-08-17 03:43:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "abasto_29870Identifier",
+  url: "https://abasto_29870Identifier.botics.co",
+  api: "https://abasto_29870Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
